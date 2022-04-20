391cfd92b1ba6c5eda782c713e0ce325612289c9
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2022-04-20 18:04:10 +0600

    build alpha

--- main.js
+++ main.js
@@ -1,48 +1,43 @@
-const { app, BrowserWindow, protocol} = require("electron");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
 const { isMac } = require("./electron/platforms");
-const databaseMigration = require('./electron/data/migration')
-require('./electron/ipc-routes')
+const databaseMigration = require("./electron/data/migration");
+require("./electron/ipc-routes");
 
 const isDev = process.env.IS_DEV == "true" ? true : false;
 
-
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        webPreferences: {
-            preload: path.join(__dirname, "preload.js"),
-            contextIsolation: true
-        },
-    });
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      webSecurity: false,
+    },
+  });
 
-    win.loadURL(isDev
-        ? 'http://localhost:3000'
-        : `file://${path.join(__dirname, '../dist/index.html')}`)
+  if (isDev) {
+    win.loadURL("http://localhost:3000");
+  } else {
+    win.loadFile(`${path.join(__dirname, "./dist/index.html")}`);
+  }
 
-    win.webContents.openDevTools();
-
-    databaseMigration()
+  databaseMigration();
 }
 
 app.whenReady().then(() => {
-    protocol.registerHttpProtocol('md-notes', (request, callback) => {
-        callback('Response');
-    })
+  createWindow();
 
-    createWindow();
-
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
-
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
 app.on("window-all-closed", () => {
-    if (! isMac()) {
-        app.quit();
-    }
+  if (!isMac()) {
+    app.quit();
+  }
 });
